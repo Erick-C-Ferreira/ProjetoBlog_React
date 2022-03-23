1130f819da88c1de6ffd125a6bd2c26c2a1d6262
Erick <erickferreira1805@gmail.com>
2022-03-23 19:16:13 -0300

    Projeto React - Parte 1 do BlogPessoal

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css"
+
+function Home(){
+    return (
+        <>
+            <h1>Home</h1>
+            <h2>Olá Turma 44</h2>
+        </>
+    )
+}
+
+export default Home
