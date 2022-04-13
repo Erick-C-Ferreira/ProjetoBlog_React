19476e890ae4bc33b5de902855efac09329a4e0a
Erick <erickferreira1805@gmail.com>
2022-04-12 21:49:20 -0300

    Inserindo atualizações

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,6 @@ import store from './store/store';
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
-
 function App() {
   return (
     <Provider store={ store }>  
